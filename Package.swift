27930ed31134eb8f0688338ed2590faff885efa1
Lukas Kollmer <hey@lukaskollmer.de>
2025-04-11 18:19:25 +0200

    update deps

--- Package.swift
+++ Package.swift
@@ -25,11 +25,11 @@ let package = Package(
         .library(name: "SpeziStudyDefinition", targets: ["SpeziStudyDefinition"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/FHIRModels", .upToNextMinor(from: "0.5.0")),
+        .package(url: "https://github.com/apple/FHIRModels.git", .upToNextMinor(from: "0.5.0")),
         .package(url: "https://github.com/StanfordSpezi/Spezi.git", .upToNextMajor(from: "1.8.1")),
-        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", branch: "lukas/platform-env-detection"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", .upToNextMajor(from: "2.1.4")),
         .package(url: "https://github.com/StanfordSpezi/SpeziHealthKit.git", .upToNextMajor(from: "1.0.1")),
-        .package(url: "https://github.com/StanfordSpezi/SpeziScheduler.git", branch: "lukas/scheduler-persistence-macos"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziScheduler.git", .upToNextMajor(from: "1.2.5")),
         .package(url: "https://github.com/StanfordSpezi/SpeziStorage.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.4"))
     ] + swiftLintPackage(),
